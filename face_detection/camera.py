efb63ef05c6a725af090d01c995b5e9082ed8f96
Alex Benko <benkoalex521@gmail.com>
2022-02-01 10:02:30 -0700

    moved to own folder

--- face_detection/camera.py
+++ face_detection/camera.py
@@ -5,12 +5,11 @@ face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
 ds_factor=0.6
 class VideoCamera(object):
     def __init__(self):
-       #capturing video
        self.video = cv2.VideoCapture(0)
 
     def __del__(self):
-        #releasing camera
         self.video.release()
+
     def get_frame(self):
       #extracting frames
       ret, frame = self.video.read()
