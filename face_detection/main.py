e416450152a69ae6ff9920d3221eb559763284b0
Alex Benko <benkoalex521@gmail.com>
2022-02-01 16:57:00 -0700

    Moved routes around and imported new Monitor class

--- face_detection/main.py
+++ face_detection/main.py
@@ -2,25 +2,26 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template, Response
 from camera import VideoCamera
+from monitor import Monitor
 import os
 
 app = Flask(__name__)
-@app.route('/')
-def index():
-    return render_template('i.html')
-
 def gen(camera):
     while True:
         frame = camera.get_frame()
         yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/')
+def index():
+    return render_template('i.html')
 
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(VideoCamera()),mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    print(os.environ.get('password'))
-    app.run(host='0.0.0.0',port='5000', debug=True)
+    app.run(host='0.0.0.0',port='5000')
 
 #docker build -t cam:latest .
 #docker tag cam alexbenko/cam && docker push alexbenko/cam
+#sudo docker run --device /dev/video0 -p 5000:5000 alexbenko/cam
