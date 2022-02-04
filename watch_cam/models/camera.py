d66731f607e0da45386f947ddcf8b2de37f17e8e
Alex Benko <benkoalex521@gmail.com>
2022-02-04 12:08:03 -0700

    Method to detect motion instead

--- watch_cam/models/camera.py
+++ watch_cam/models/camera.py
@@ -8,8 +8,8 @@ import time
 face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
 ds_factor=0.6
 class VideoCamera(object):
-    images_folder_path= '../recordings'
-    save_images = True
+    images_folder_path= './recordings'
+    save_images = False
     def __init__(self):
        self.video = cv2.VideoCapture(0)
 
@@ -28,6 +28,7 @@ class VideoCamera(object):
       #date is expected to be in: yyyy-mm-dd ie datetime.date.today()
       image_folder = f'{self.images_folder_path}/{date}'
       video_name = f'{self.images_folder_path}/{date}/{date}.avi'
+
       images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
       frame = cv2.imread(os.path.join(image_folder, images[0]))
       height, width, layers = frame.shape
@@ -39,7 +40,7 @@ class VideoCamera(object):
 
       cv2.destroyAllWindows()
       video.release()
-    def get_frame(self):
+    def detect_human_faces(self):
       #extracting frames
       ret, frame = self.video.read()
       frame=cv2.resize(frame,None,fx=ds_factor,fy=ds_factor,interpolation=cv2.INTER_AREA)
@@ -60,3 +61,37 @@ class VideoCamera(object):
         self.save_image(frame)
 
       return jpeg.tobytes()
+
+    def detect_motion(self,baseline_image=None):
+      ret, frame = self.video.read()
+      frame=cv2.resize(frame,None,fx=ds_factor,fy=ds_factor,interpolation=cv2.INTER_AREA)
+
+      gray_frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+      gray_frame=cv2.GaussianBlur(gray_frame,(25,25),0)
+
+      if baseline_image is None:
+        #this means it needs to grab first image
+        baseline_image=gray_frame
+        return baseline_image
+
+      delta=cv2.absdiff(baseline_image,gray_frame)
+      threshold=cv2.threshold(delta,35,255, cv2.THRESH_BINARY)[1]
+      (contours,_)=cv2.findContours(threshold,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+      for contour in contours:
+        if cv2.contourArea(contour) < 3000:
+          continue
+        (x, y, w, h)=cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0), 1)
+
+      timestamp = datetime.datetime.now()
+      cv2.putText(frame, timestamp.strftime("%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0,255,0), 1)
+      # encode OpenCV raw frame to jpg and displaying it
+      ret, jpeg = cv2.imencode('.jpg', frame)
+
+      if self.save_images:
+        self.save_image(frame)
+
+      return jpeg.tobytes()
+
+
