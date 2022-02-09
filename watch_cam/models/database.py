5dc2c33252cefc41d3d24349d4e896e942ad1ba2
Alex Benko <benkoalex521@gmail.com>
2022-02-09 11:17:44 -0700

    inital database class set up for mongodb

--- watch_cam/models/database.py
+++ watch_cam/models/database.py
@@ -1,7 +1,39 @@
-import pymongo
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+import json
 class Database(object):
   def __init__(self):
-    self.client = pymongo.MongoClient
+    try:
+      load_dotenv()
+      #user = os.getenv("MONGODB_USERNAME")
+      #password = os.getenv("MONGODB_PASSWORD")
+      self.db_name = os.getenv("MONGODB_DATABASE")
+      connection_str = f"mongodb://localhost:27017"
+
+      self.client = MongoClient(connection_str)
+      print("Successfully connected to MongoDb")
+
+    except Exception as error:
+      print("Failed to Connect to MongoDb\n")
+      print(error)
+
   def __del__(self):
     self.client.close()
 
+  def seed_ips(self):
+    # when i set up port forwards for this app on my router, a decent amount of unwanted people tried to access my livestream app
+    # luckily my internet provider blocked them and told me their ips, so i have been storing them in a json file so i can implement an ip banner/checker
+    banned_ips = self.client[self.db_name]["ips"]
+    ips = list(banned_ips.find({}))
+
+    if len(ips) == 0:
+      with open('../data/ips.json') as file:
+        file_data = json.load(file)
+        banned_ips.insert_many(file_data)
+    else:
+      print('Data exisits, no need to seed!')
+
+  def get_one(self, collection, value):
+    collection = self.client[self.db_name][collection]
+    return collection.find_one(value)
