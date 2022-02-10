b7642a314fd4dab650fec695b78e66093dcafebc
Alex Benko <benkoalex521@gmail.com>
2022-02-10 09:05:30 -0700

    creates unqiue index, and removed __del__

--- watch_cam/models/database.py
+++ watch_cam/models/database.py
@@ -1,4 +1,4 @@
-from pymongo import MongoClient
+from pymongo import MongoClient,DESCENDING
 import os
 from dotenv import load_dotenv
 import json
@@ -9,7 +9,8 @@ class Database(object):
       #user = os.getenv("MONGODB_USERNAME")
       #password = os.getenv("MONGODB_PASSWORD")
       self.db_name = os.getenv("MONGODB_DATABASE")
-      connection_str = f"mongodb://localhost:27017"
+      host = os.getenv("MONGODB_HOSTNAME", "localhost")
+      connection_str = f"mongodb://{host}:27017"
 
       self.client = MongoClient(connection_str)
       print("Successfully connected to MongoDb")
@@ -18,9 +19,6 @@ class Database(object):
       print("Failed to Connect to MongoDb\n")
       print(error)
 
-  def __del__(self):
-    self.client.close()
-
   def seed_ips(self):
     # when i set up port forwards for this app on my router, a decent amount of unwanted people tried to access my livestream app
     # luckily my internet provider blocked them and told me their ips, so i have been storing them in a json file so i can implement an ip banner/checker
@@ -28,9 +26,12 @@ class Database(object):
     ips = list(banned_ips.find({}))
 
     if len(ips) == 0:
-      with open('../data/ips.json') as file:
+      print("Seeding Banned Ip Collection...")
+      with open('./data/ips.json') as file:
         file_data = json.load(file)
       banned_ips.insert_many(file_data)
+      banned_ips.create_index([("ip", DESCENDING)],unique=True)
+      print("Done Seeding!")
     else:
       print('Data exisits, no need to seed!')
 
