595833082f8d7e4c75d5769f1d66db81c0d54cce
Alex Benko <benkoalex521@gmail.com>
2022-03-02 10:15:11 -0700

    Uses port env and fixed bug with streaming face detection

--- watch_cam/main.py
+++ watch_cam/main.py
@@ -29,7 +29,6 @@ def motion_detection(camera):
     frame = camera.detect_motion()
     yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-CAMERA = VideoCamera()
 app = Flask(__name__,static_url_path='/static')
 
 @app.before_request
@@ -91,12 +90,14 @@ def cam(mode):
     return error_responses["404"], 404
 
 @app.route('/face_feed')
-def video_feed():
-  return Response(face_detection(CAMERA),mimetype='multipart/x-mixed-replace; boundary=frame')
+def face_feed():
+  camera = VideoCamera()
+  return Response(face_detection(camera),mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route("/motion_feed")
+@app.route('/motion_feed')
 def motion_feed():
-  return Response(motion_detection(CAMERA),mimetype='multipart/x-mixed-replace; boundary=frame')
+  camera = VideoCamera()
+  return Response(motion_detection(camera),mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/play/<file>', methods = ['POST'])
 def play(file):
@@ -126,10 +127,10 @@ if __name__ == '__main__':
 
   print(f'Only local access: {local_access_only}')
   print(f'Save images: {save_images}')
-
+  port = os.getenv("PORT", "5000")
   timestamp = datetime.datetime.now()
   sms.send(f"Server started at {timestamp}")
-  app.run(host='0.0.0.0',port='5000')
+  app.run(host='0.0.0.0',port=port)
 
 #docker build -t cam:latest .
 #docker tag cam alexbenko/cam && docker push alexbenko/cam
